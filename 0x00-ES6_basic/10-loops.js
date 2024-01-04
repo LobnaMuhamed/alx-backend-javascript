21c0cb222f52ee1558e80ce6b6c43744080b0f15
LobnaMuhamed <mohamedlobna306@gmail.com>
2024-01-03 18:59:16 -0800

    fix task

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,11 +1,8 @@
-export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-    const task = true; // eslint-disable-line no-unused-vars
-    const task2 = false; // eslint-disable-line no-unused-vars
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
-  return [task, task2];
+  return newArray;
 }
